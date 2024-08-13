24bb1f35bb55354188205829707c6bc51123891a
KroSheChKa <104899233+KroSheChKa@users.noreply.github.com>
2024-08-13 22:26:26 +0500

    Update Markdown.py

--- Markdown.py
+++ Markdown.py
@@ -8,8 +8,9 @@ mrkd_tps = {
     'strikethrough': '~',
     'code': '`',
     'spoiler': '||',
+    'pre': '```'
 }
-
+special = ['blockquote', 'mention', 'text_link', ('expandable_blockquote', ['**>', '||'])]
 stupid = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']
 
 bot = t.TeleBot(TOKEN)
@@ -21,37 +22,74 @@ def welcome(msg):
 @bot.message_handler()
 def info(msg):
     markdowned = msg.text
-    print(markdowned)
-    for i in stupid:
-        markdowned = markdowned.replace(i, '\\'+i)
-    print(markdowned)
+
     if 'entities' in msg.json:
         markdowned = parse_dat_bih(markdowned, msg.json['entities'])
         #print(msg.json['entities'])
 
     bot.send_message(msg.chat.id, f'{markdowned}', disable_web_page_preview=True)
     bot.send_message(msg.chat.id, f'{markdowned}', disable_web_page_preview=True, parse_mode='MarkdownV2')
-
+    #bot.send_message(msg.chat.id, 'hello', disable_web_page_preview=True, parse_mode='MarkdownV2')
 
 def parse_dat_bih(text, ent):
-    to_add = 0
-    old_offset = -1
-    for el in ent:
-        length = el['length']
-        type_ = el['type']
-        offset = el['offset']
-        if old_offset != offset:
-            offset += to_add
-        else:
-            length += len(mrkd_tps[type_])*2
-        if type_ == 'text_link':
-            text = text[:offset]+'['+text[offset:offset+length]+']('+el['url']+')'+text[offset+length:]
-            to_add += 4
-        else:
-            text = text[:offset]+mrkd_tps[type_]+text[offset:offset+length]+mrkd_tps[type_]+text[offset+length:]
-            to_add += len(mrkd_tps[type_])*2
-        print(el)
-        old_offset = el['offset']
+    places = place(ent)
+    splitted_dots = dots(text, places)
+    symbols = symbols_to_paste(ent, text)
+    text = combine(splitted_dots, symbols)
     return text
+
+def place(ent):
+    s = set()
+    for i in ent:
+        s.add(i['offset'])
+        s.add(i['offset']+i['length'])
+    s = sorted(list(s))
+    print(s)
+    return s
+
+def dots(text, places):
+    text = [i for i in text]
+    to_add = 0
+    for i in places:
+        text.insert(i+to_add, '•')
+        to_add += 1
+    print(text)
+    return text
+
+def symbols_to_paste(ent, text):
+    s = ['']*(len(text)+1)
+    for i in ent:
+        if i['type'] == 'text_link':
+            s[i['offset']] += '['
+            s[i['length'] + i['offset']] += '](' + i['url'] + ')'
+            continue
+        print(i)
+        s[i['offset']] += mrkd_tps[i['type']]
+        s[i['length'] + i['offset']] = mrkd_tps[i['type']] + s[i['length'] + i['offset']]
+    s = [j for j in s if j]
+    print(s,'1')
+    flag = 0
+    for i in range(len(s)):
+        if '___' in s[i]:
+            if flag:
+                s[i] = s[i].replace('___', '_**__')
+            else:
+                s[i] = s[i].replace('___', '__**_')
+            flag = not flag
+    print(s, 'ya')
+    return s
+
+def combine(splitted_dots, symbols):
+    splitted_dots = "".join(splitted_dots)
+
+    for i in stupid:
+        splitted_dots = splitted_dots.replace(i, '\\'+i)
+
+    print(splitted_dots,'2')
+    for i in symbols:
+        splitted_dots = splitted_dots.replace('•', i, 1)
+    print(splitted_dots)
+    return splitted_dots
+
 
 bot.infinity_polling()
